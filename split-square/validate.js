0f5fe02088eb21145b0473f24ab401656700c46f
ivoriTal <s21italiferro@gmail.com>
2024-08-11 20:11:12 -0400

    first commit

---
+++ split-square/validate.js
@@ -0,0 +1,14 @@
+
+
+function is_valid(s) {
+  if (s === 0 || s === 1) {
+    return true;
+  }
+
+  if (Array.isArray(s) && s.length === 4) {
+    
+    return s.every(is_valid);
+  }
+
+  return false;
+}
